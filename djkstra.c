507d3fc56f5af95ebdb72f04b3b34dd880968e7a
Adam Syed <asyed@e1z3r5p10.42.us.org>
2018-01-12 16:03:33 -0800

    A lot more optimized

--- djkstra.c
+++ djkstra.c
@@ -6,7 +6,7 @@
 /*   By: asyed <asyed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/09 22:07:38 by asyed             #+#    #+#             */
-/*   Updated: 2018/01/12 15:08:18 by asyed            ###   ########.fr       */
+/*   Updated: 2018/01/12 16:03:23 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ int	node_weight(t_rooms *node)
 	tmp = node;
 	while (tmp->path)
 	{
+		// printf("%s prev = %s\n", node->name, tmp->path->name);
 		weight += path_weight(tmp->path, tmp);
 		tmp = tmp->path;
 	}
@@ -51,6 +52,11 @@ int	dij_start(t_input **input_d)
 	while (neighbors)
 	{
 		neighbors->end->path = node;
+		neighbors = neighbors->next;
+	}
+	neighbors = node->to_link;
+	while (neighbors)
+	{
 		neighbor_update(neighbors->end, input_d);
 		neighbors = neighbors->next;
 	}
@@ -71,10 +77,63 @@ int	neighbor_update(t_rooms *node, t_input **input_d)
 				node_weight(neighbors->end) > n_weight + path_weight(node, neighbors->end)))
 		{
 			neighbors->end->path = node;
-			if (neighbors->end != (*input_d)->dest)
-				neighbor_update(neighbors->end, input_d);
+			// neighbor_update(neighbors->end, input_d);
 		}
 		neighbors = neighbors->next;
+	}
+	return (1);
+}
+
+int	min_neighbor(t_rooms *node, t_input **input_d)
+{
+	// t_links	*min_neighbor;
+	t_links	*neighbors;
+	int		n_weight;
+	int		ne_weight;
+	int		p_weight;
+	int		modified;
+
+	printf("Stuck on node_weight()\n");
+	n_weight = node_weight(node);
+	printf("or am I?!\n");
+	neighbors = node->to_link;
+	modified = 0;
+	while (neighbors)
+	{
+		printf("Fetching node_weight()\n");
+		ne_weight = node_weight(neighbors->end);
+		printf("Fetching path_weight()\n");
+		p_weight = path_weight(node, neighbors->end);
+		if (neighbors->end != (*input_d)->origin &&
+			n_weight != INFINITY &&
+			ne_weight > n_weight + p_weight)
+		{
+			printf("(M:%s N:%s) %d > %d + %d\n", node->name, neighbors->end->name, ne_weight, n_weight, p_weight);
+			neighbors->end->path = node;
+			modified++;
+		}
+		neighbors = neighbors->next;
+	}
+	return (modified);
+}
+
+int	list_update(t_rooms *node, t_input **input_d)
+{
+	int		res;
+	t_rooms	*tmp;
+	
+	res = 1;
+	while (node && res)
+	{
+		printf("==========Next itteration!!!!=======\n");
+		res = 0;
+		tmp = node;
+		while (tmp)
+		{
+			printf("Next neighbor(%s)\n", tmp->name);
+			res += min_neighbor(tmp, input_d);
+			tmp = tmp->next;
+		}
 	}
 	return (1);
 }
@@ -89,6 +148,7 @@ int	dijkstra(t_input **input_d)
 		printf("Failed to initialize neighbors from starting room.\n");
 		return (0);
 	}
+	list_update(node, input_d);
 	printf("Done :)\n");
 	printf("\n========== OP =========\n");
 	printf("Optimal path to |\"%s\"|", (*input_d)->dest->name);
